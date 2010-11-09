a14e4f3f4b7b4f004a423a9b9cd6d745d354b7b5
Simao Belchior de Castro <simao.belchior@unep-wcmc.org>
2010-11-09 13:25:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddDemo::Application.config.secret_token = 'b43c98dd140481824fe665ea14cbd6cec594058d28d925b30c022437ba1909e6009f1cb0528a1da679ca3f8a2ee81b436d22e94934e3e9f543b6490ccfed66e1'
